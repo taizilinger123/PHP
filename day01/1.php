402b44811585e11bb9545beba6aec12286c7a5a0
taizlinger123 <837337164@qq.com>
2019-07-25 21:59:46 +0800

    this is a php

---
+++ day01/1.php
@@ -0,0 +1,9 @@
+﻿<?php
+//header("content-type:text/html;charset=utf-8");
+$name = '李四';
+function say()
+{
+  $name = '张三';
+}
+
+echo  $name;
---
+++ day01/2.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+$arr = ['a','b','c','d'];
+
+//var_dump($arr); //var_dump打印值和数据类型
+//获取数组的值
+echo $arr[0];
+
+//添加一个元素
+$arr[4] = '这是我新添加的';
+
+var_dump($arr);
+
