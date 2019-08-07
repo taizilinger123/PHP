df07fbe73fead048a82db06b262498ace121cd3d
taizlinger123 <837337164@qq.com>
2019-08-07 22:23:49 +0800

    2019-8-7

---
+++ day04/userlist.php
@@ -0,0 +1,32 @@
+<?php
+$link = mysqli_connect('localhost','root','123456');
+
+if(!$link){
+	exit('数据库连接失败');
+}
+
+mysqli_set_charset($link, 'utf8');
+
+mysqli_select_db($link, 'bbs');  
+
+$sql = "select * from bbs_user";
+
+$obj = mysqli_query($link, $sql);
+
+echo '<table width="600" border="1">';
+      echo '<th>编号</th><th>用户名</th><th>地址</th><th>性别</th><th>年龄</th><th>操作</th>';
+	  
+	  while($rows = mysqli_fetch_assoc($obj)){
+		  echo '<tr>';
+		       echo '<td>'.$rows['id'].'</td>';
+			   echo '<td>'.$rows['username'].'</td>';
+			   echo '<td>'.$rows['address'].'</td>';
+			   echo '<td>'.$rows['sex'].'</td>';
+			   echo '<td>'.$rows['age'].'</td>';
+			   echo '<td><a href="del.php?id='.$rows['id'].'">删除</a>/<a href="update.php?id='.$rows['id'].'">修改</a></td>';
+		  echo '</tr>';
+		  
+	  }
+echo '</table>';
+
+mysqli_close($link);
